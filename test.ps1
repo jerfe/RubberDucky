830f6a16054ff648326736c77907523c20940ca3
jerfe <38888431+jerfe@users.noreply.github.com>
2023-02-08 00:18:05 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,3 @@
 whoami /all
-echo 'Your computer has been pwned! You will never know if malware persistence is remaining in the background because you just forgot to lock your session...'; pause
+echo 'Your computer has been pwned! You will never know if malware persistence is remaining in the background because you just forgot to lock your session...'
+pause
