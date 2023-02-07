ff7fa6e15bcba85fcccc1604bf88ec952c9cb3db
jerfe <38888431+jerfe@users.noreply.github.com>
2023-02-08 00:14:49 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,2 @@
-echo 'Your computer has been powned! You will never know if malware persistence is remaining in the background because you just forgot to lock your session...'; pause
+whoami /all
+echo 'Your computer has been pwned! You will never know if malware persistence is remaining in the background because you just forgot to lock your session...'; pause
